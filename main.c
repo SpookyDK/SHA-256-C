87d7379e996912179b5c9fc5c885d9a00744c558
SkraemmendeDanmark <esben9541@gmail.com>
2026-06-05 13:59:14 +0200

    It fucking works and i accurate

--- main.c
+++ main.c
@@ -6,20 +6,17 @@
 #define rotate_right32(data, amount) (((data) >> (amount)) | ((data) << (32 - (amount))))
 #define rotate_left32(data, amount) (((data) << (amount)) | ((data) >> (32 - (amount))))
 
-int pad_message(const char *mymessage, const uint32_t message_len, char **out, uint32_t *outlen, uint32_t *outchunks) {
-    uint64_t size_b = message_len * 8;
+int pad_message(const u_char *mymessage, const uint32_t message_len, u_char **out, uint32_t *outlen, uint32_t *outchunks) {
+    uint64_t size_b = (uint64_t)message_len * 8;
     printf("Mes lenb = %d\n", size_b);
     uint32_t nearest = (size_b + 1 + 64 + 511) & ~(512 - 1);
     uint32_t nearest_byte = nearest / 8;
-    char *padded_message = malloc(nearest_byte);
+    char *padded_message = calloc(1, nearest_byte);
     if (padded_message == NULL)
         return -1;
     printf("nearest 512 = %d\n", nearest);
     memcpy(padded_message, mymessage, message_len);
     padded_message[message_len] = 0b10000000;
-    for (int i = message_len + 1; i < nearest_byte - 8; i++) {
-        padded_message[i] = 0;
-    }
     padded_message[nearest_byte - 8] = (size_b >> 56) & 0xFF;
     padded_message[nearest_byte - 7] = (size_b >> 48) & 0xFF;
     padded_message[nearest_byte - 6] = (size_b >> 40) & 0xFF;
@@ -46,8 +43,8 @@ static const uint32_t K[64] = {
 static const uint32_t H[8] = {
     0x6a09e667, 0xbb67ae85, 0x3c6ef372, 0xa54ff53a, 0x510e527f, 0x9b05688c, 0x1f83d9ab, 0x5be0cd19,
 };
-int process_chunk(char *const padded_message_chunk, uint32_t chunk_len) {
-    return 1;
+
+int process_chunk(u_char *const padded_message_chunk, uint32_t chunk_len, uint32_t *h_values) {
 
     if (chunk_len != 512) {
         printf("chunk_len shall always be 512\n2");
@@ -57,31 +54,33 @@ int process_chunk(char *const padded_message_chunk, uint32_t chunk_len) {
 
     // We need 64 words of 32 bit
     uint32_t hashed_array[64];
-    // THe chunk is 16 words which we copy into the first 16 words of our array
-    memcpy(hashed_array, padded_message_chunk, 512 / 8);
+    // THe chunk is 16 words which we copy into the first 16 words of our array in big endian bullshit
+    for (int i = 0; i < 16; i++) {
+        hashed_array[i] = ((uint32_t)(unsigned char)padded_message_chunk[i * 4] << 24) |
+                          ((uint32_t)(unsigned char)padded_message_chunk[i * 4 + 1] << 16) |
+                          ((uint32_t)(unsigned char)padded_message_chunk[i * 4 + 2] << 8) |
+                          ((uint32_t)(unsigned char)padded_message_chunk[i * 4 + 3]);
+    }
     // Now we need to expand, so we fill out the 64 chunks
     // W16 = (w0+simga0+w9+sigma1)
     for (int i = 16; i < 64; i++) {
+        printf("word %d\n", i);
         uint32_t sigma0 = rotate_right32(hashed_array[i - 15], 7) ^ rotate_right32(hashed_array[i - 15], 18) ^ (hashed_array[i - 15] >> 3);
-        uint32_t sigma1 = rotate_right32(hashed_array[i - 1], 17) ^ rotate_right32(hashed_array[i - 1], 19) ^ (hashed_array[i - 1] >> 10);
-        hashed_array[i] = (hashed_array[i - 16] + sigma0 + hashed_array[i - 9] + sigma1);
+        uint32_t sigma1 = rotate_right32(hashed_array[i - 2], 17) ^ rotate_right32(hashed_array[i - 2], 19) ^ (hashed_array[i - 2] >> 10);
+        hashed_array[i] = (hashed_array[i - 16] + sigma0 + hashed_array[i - 7] + sigma1);
     }
+    uint32_t a = h_values[0], b = h_values[1], c = h_values[2], d = h_values[3];
+    uint32_t e = h_values[4], f = h_values[5], g = h_values[6], h = h_values[7];
     // We initialze our working variables
-    uint32_t a = 0x6a09e667;
-    uint32_t b = 0xbb67ae85;
-    uint32_t c = 0x3c6ef372;
-    uint32_t d = 0xa54ff53a;
-    uint32_t e = 0x510e527f;
-    uint32_t f = 0x9b05688c;
-    uint32_t g = 0x1f83d9ab;
-    uint32_t h = 0x5be0cd19;
     for (int i = 0; i < 64; i++) {
         uint32_t majority = (a & b) ^ (a & c) ^ (b & c);
         uint32_t sum0 = rotate_right32(a, 2) ^ rotate_right32(a, 13) ^ rotate_right32(a, 22);
         uint32_t sum1 = rotate_right32(e, 6) ^ rotate_right32(e, 11) ^ rotate_right32(e, 25);
         uint32_t choice = (e & f) ^ ((~e) & g);
+
         uint32_t temp1 = h + sum1 + choice + K[i] + hashed_array[i];
         uint32_t temp2 = sum0 + majority;
+
         h = g;
         g = f;
         f = e;
@@ -91,28 +90,50 @@ int process_chunk(char *const padded_message_chunk, uint32_t chunk_len) {
         b = a;
         a = temp1 + temp2;
     }
-    // we add our final working variables to the constant h values
-    uint32_t sha256hash[8];
-    sha256hash[0] = H[0] + a;
-    sha256hash[1] = H[1] + b;
-    sha256hash[2] = H[2] + c;
-    sha256hash[3] = H[3] + d;
-    sha256hash[4] = H[4] + e;
-    sha256hash[5] = H[5] + f;
-    sha256hash[6] = H[6] + g;
-    sha256hash[7] = H[7] + h;
+    h_values[0] += a;
+    h_values[1] += b;
+    h_values[2] += c;
+    h_values[3] += d;
+    h_values[4] += e;
+    h_values[5] += f;
+    h_values[6] += g;
+    h_values[7] += h;
+    return 0;
 }
-
-int do_sha(const char *mymessage, const uint32_t message_len, char *out) {
-    char *padded_message = NULL;
+int sha256_hash(const u_char *mymessage, const uint32_t message_len, char **out, uint32_t *outlen) {
+    u_char *padded_message = NULL;
     uint32_t padded_length = 0;
     uint32_t chunks = 0;
     pad_message(mymessage, message_len, &padded_message, &padded_length, &chunks);
-
+    uint32_t working_values[8];
+    memcpy(working_values, H, 8 * sizeof(uint32_t));
+    for (int i = 0; i < chunks; i++) {
+        process_chunk(&padded_message[i * 64], 512, working_values);
+        // we add our final working variables to the constant h values and contionue
+    }
+    unsigned char *sha256hash = malloc(32);
+    for (int i = 0; i < 8; i++) {
+        sha256hash[i * 4] = (working_values[i] >> 24) & 0xFF; // Most significant byte
+        sha256hash[i * 4 + 1] = (working_values[i] >> 16) & 0xFF;
+        sha256hash[i * 4 + 2] = (working_values[i] >> 8) & 0xFF;
+        sha256hash[i * 4 + 3] = working_values[i] & 0xFF; // Least significant byte
+    }
+    *out = (char *)sha256hash;
+    *outlen = 32;
+    free(padded_message);
     return 0;
 }
 int main() {
 
     char mymessage[] = "I Love You";
-    do_sha(mymessage, sizeof(mymessage), NULL);
+    char *hashmessage;
+    uint32_t outlen;
+    sha256_hash((u_char *)mymessage, strlen(mymessage), &hashmessage, &outlen);
+    printf("outlen %d\n", outlen);
+    printf("Your Hash: ");
+    for (int i = 0; i < outlen; i++) {
+        // FIX 2: Use standard unsigned char and force a 2-digit hex format
+        printf("%02x", (unsigned char)hashmessage[i]);
+    }
+    printf("\n");
 }
